d4a0ddc551d8b92d5f3c8d5b1170763733a1b1f0
gabriellakodra <gabriella.kodra@gmail.com>
2026-03-16 15:03:18 +0200

    database for doing posts and simple post function

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    db = sqlite3.connect("database.db")
+    posts = db.execute("SELECT content FROM posts").fetchall()
+    db.close()
+    count = len(posts)
+    return render_template("index.html", count=count, posts=posts)
+
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    db = sqlite3.connect("database.db")
+    db.execute("INSERT INTO posts (content) VALUES (?)", [content])
+    db.commit()
+    db.close()
+    return redirect("/")
 
 
 @app.route("/form")
