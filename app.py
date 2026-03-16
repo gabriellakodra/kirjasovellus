612328c1e4fed72e07ba6a77dc29fd0802429528
gabriellakodra <gabriella.kodra@gmail.com>
2026-03-16 14:23:17 +0200

    add more pages

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "Heipparallaa!"
+
+
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1"
+
+
+@app.route("/page2")
+def page2():
+    return "Tämä on sivu 2"
