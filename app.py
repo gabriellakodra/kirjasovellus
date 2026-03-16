b59151f4639201c76765a2f8279394fddaeb3a9e
gabriellakodra <gabriella.kodra@gmail.com>
2026-03-16 14:39:13 +0200

    form for message and results pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    message = request.form["message"]
+    return render_template("result.html", message=message)
 
 
 @app.route("/page1")
