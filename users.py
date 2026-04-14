3d094be562a1db0d6b667a248186a5d60b9e89e9
gabriellakodra <gabriella.kodra@gmail.com>
2026-04-14 11:47:12 +0300

    profile picture to user

--- users.py
+++ users.py
@@ -2,7 +2,7 @@ import db
 
 
 def get_user(user_id):
-    sql = "SELECT id, username FROM users WHERE id = ?"
+    sql = "SELECT id, username, image IS NOT NULL has_image FROM users WHERE id = ?"
     result = db.query(sql, [user_id])
     return result[0] if result else None
 
@@ -27,3 +27,14 @@ def get_comments(user_id):
                    c.user_id = ?
              ORDER BY c.sent_at DESC"""
     return db.query(sql, [user_id])
+
+
+def update_image(user_id, image):
+    sql = "UPDATE users SET image = ? WHERE id = ?"
+    db.execute(sql, [image, user_id])
+
+
+def get_image(user_id):
+    sql = "SELECT image FROM users WHERE id = ?"
+    result = db.query(sql, [user_id])
+    return result[0][0] if result else None
